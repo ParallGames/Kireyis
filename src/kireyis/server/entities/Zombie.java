4af8236e3e30297621cf4705a9bbd609a60b577f
valentin616 <valchap616@gmail.com>
2019-04-17 17:25:31 +0200

    Zombie attack player

--- src/kireyis/server/entities/Zombie.java
+++ src/kireyis/server/entities/Zombie.java
@@ -7,6 +7,8 @@ import kireyis.server.World;
 public class Zombie extends LivingEntity {
 	private static final double ACCELERATION = 0.001;
 
+	private int attackTimer = 0;
+
 	public Zombie(final int x, final int y) {
 		this.x = x;
 		this.y = y;
@@ -50,5 +52,17 @@ public class Zombie extends LivingEntity {
 		this.speedX += Math.cos(angle) * ACCELERATION;
 
 		this.speedY += Math.sin(angle) * ACCELERATION;
+
+		if (attackTimer > 0) {
+			attackTimer--;
+		}
+	}
+
+	@Override
+	public void collideWith(final Entity e) {
+		if (attackTimer == 0 && e instanceof Player) {
+			((Player) e).damage(1);
+			attackTimer = 100;
+		}
 	}
 }
